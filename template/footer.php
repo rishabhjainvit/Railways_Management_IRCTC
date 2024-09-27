25219caba01ebe3d401390d39319edc1ebc0aba1
Rishabh Jain <158610354+rishabhjainvit@users.noreply.github.com>
2024-09-28 03:48:38 +0530

    updated and changes made

---
+++ template/footer.php
@@ -0,0 +1,8 @@
+<footer>
+  <div class="footer bg-light">
+
+    <p>© Copyright 2020 <a href = "https://github.com/pranjalibajpai/railway-reservation-system">Github Repo</a></p>
+    
+  </div>
+</footer>
+</body>
